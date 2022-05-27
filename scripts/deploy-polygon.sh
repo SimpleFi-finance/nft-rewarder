cb9b381628a62de2421ca923aef2c7d03ceb8b09
Goran Vladika <goran.vladika@gmail.com>
2022-05-27 10:25:49 +0200

    Improve deployment scripts

--- scripts/deploy-polygon.sh
+++ scripts/deploy-polygon.sh
@@ -3,7 +3,6 @@
 source .env
 
 echo Deploying Simplefi NFT rewarder to Polygon
-forge build --contracts ./src/
-forge create ./src/NFTRewarder.sol:NFTRewarder --use 0.8.12 --rpc-url $POLYGON_RPC_URL --private-key $PRIVATE_KEY --gas-price $ETH_GAS_PRICE --priority-fee $ETH_GAS_PRIORITY_FEE --constructor-args $WHITELISTER
+forge build --force --contracts ./src/
+forge create ./src/NFTRewarder.sol:NFTRewarder --use 0.8.12 --rpc-url $POLYGON_RPC_URL --private-key $PRIVATE_KEY --gas-price $POLYGON_GAS_PRICE --priority-fee $POLYGON_GAS_PRIORITY_FEE --constructor-args $WHITELISTER
 echo Done!
-
--- scripts/verify-polygon.sh
+++ scripts/verify-polygon.sh
@@ -4,7 +4,8 @@ source .env
 
 echo Verify Simplefi NFT rewarder contract
 
-forge verify-contract --chain-id 137 --constructor-args $(cast abi-encode "constructor(address)" $WHITELISTER) --compiler-version v0.8.12+commit.f00d7308 0xad9527406dDe96f1e92e3Cc699CCD24ace786bd5 ./src/NFTRewarder.sol:NFTRewarder $POLYSCAN_API_KEY
+CONTRACT_TO_VERIFY="0x1b09461084b567df0ddf65967d2e5bf3b3699b19"
+forge verify-contract --chain-id 137 --constructor-args $(cast abi-encode "constructor(address)" $WHITELISTER) --compiler-version v0.8.12+commit.f00d7308 $CONTRACT_TO_VERIFY ./src/NFTRewarder.sol:NFTRewarder $POLYSCAN_API_KEY
 
 echo Done!
 
